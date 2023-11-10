702e727f80e442e8bc5ec7a4d8d36fe4307a767b
shtanriverdi <shtanriverdi@yahoo.com>
2023-11-10 10:47:28 +0300

    numSides

--- React Props/src/Die.jsx
+++ React Props/src/Die.jsx
@@ -1,6 +1,6 @@
 export default function Die({ numSides }) {
     const roll = Math.floor(Math.random() * numSides) + 1;
     return (
-        <p>Die roll: {roll}</p>
+        <p>{numSides}-sided die roll: {roll}</p>
     );
 }
