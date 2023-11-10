03720acda253ee0dda3d20bec98a19891a4f3d73
shtanriverdi <shtanriverdi@yahoo.com>
2023-11-10 11:49:09 +0300

    Creating Dynamic Styles

--- React Props/src/DoubleDice.jsx
+++ React Props/src/DoubleDice.jsx
@@ -2,12 +2,19 @@
 export default function DoubleDice() {
     const num1 = Math.floor(Math.random() * 3) + 1;
     const num2 = Math.floor(Math.random() * 3) + 1;
+    // To create inline styles, we need to pass an object
+    const isWinner = num1 === num2;
+    const style = {
+        color: isWinner ? 'green' : 'red',
+        fontSize: isWinner ? '25px' : '20px'
+    };
     return (
-        <div>
+        <div style={style}>
             <h3>Double Dice</h3>
-            {num1 === num2 && <h2>You Win!</h2>}
+            {isWinner && <h2>You Win!</h2>}
             <p>Num1: {num1}</p>
             <p>Num2: {num2}</p>
+            <hr />
         </div>
     );
 }
