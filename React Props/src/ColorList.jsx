dd6e398a53e5f8222fd6cbe626ac59229f399091
shtanriverdi <shtanriverdi@yahoo.com>
2023-11-10 19:40:07 +0300

    Common patter, render array of elements

--- React Props/src/ColorList.jsx
+++ React Props/src/ColorList.jsx
@@ -1,11 +1,16 @@
 function ColorList({ colors }) {
     // const elements = [<p>Hello!</p>, <h2>Bye</h2>, <input type="password"/>];
-    const lis = colors.map(color => <li>{color}</li>);
+    // const lis = colors.map(color => <li>{color}</li>);
     return (
         <div>
             <p>Color List</p>
             <p>{colors}</p>
-            <ul>{lis}</ul>
+            <ul>
+                {colors.map(c => (
+                    <li style={{ color: c }}>{c}</li>
+                    // <li style={{ c }}>{c}</li>
+                ))}
+            </ul>
         </div>
     );
 }
