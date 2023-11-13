64103ab3972467582bc44d5e3709ee0125701f98
shtanriverdi <shtanriverdi@yahoo.com>
2023-11-13 20:39:11 +0300

    UUID create unique ids

--- state-demo/src/EmojiClicker.jsx
+++ state-demo/src/EmojiClicker.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import { v4 as uuid } from "uuid";
 
 export default function EmojiClicker() {
-    const [emojis, setEmojis] = useState(['⛳']);
+    // Set the random unique ID
+    const [emojis, setEmojis] = useState([{ id: uuid(), emoji: '⛳' }]);
     const addEmoji = () => {
         setEmojis((previousEmojis) => {
-            const newEmojis = [...previousEmojis, '🌏'];
+            const newEmojis = [...previousEmojis, { id: uuid(), emoji: '🌏' }];
             return newEmojis;
         });
     }
@@ -12,8 +14,11 @@ export default function EmojiClicker() {
     return (
         <>
             <div>
-                {emojis.map((emoji, index) => (
+                {/* {emojis.map((emoji, index) => (
                     <span key={index} style={{ fontSize: '4rem' }}>{emoji}</span>
+                ))} */}
+                {emojis.map((e) => (
+                    <span key={e.id} style={{ fontSize: '4rem' }}>{e.emoji}</span>
                 ))}
             </div>
             <button onClick={addEmoji}>Add Emoji</button>
