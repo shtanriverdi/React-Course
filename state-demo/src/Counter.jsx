947772e794a4a10141f4dfa8c2324752d6943dd3
shtanriverdi <shtanriverdi@yahoo.com>
2023-11-13 19:36:02 +0300

    Important notes

--- state-demo/src/Counter.jsx
+++ state-demo/src/Counter.jsx
@@ -7,18 +7,26 @@ function Counter() {
     }
     const addThree = () => {
         /* Correct Way to Update The State
+        Use This if we depend on the previous state when we update the current version state
         anytime the state depends on the old version!
         Always use this callback format!
         React will automatically queue these up
         and pass the updated state value through the others
+        But we still have previous version, because they will batch together
          */
         setCount((currentCount) => currentCount + 1);
         //        ^----------^ -> Will automatically 
         //         filled by react with old
+        setCount((currentCount) => {
+            // Still we don't have updated currentCount here as well!
+            return currentCount + 1;
+        });
+        console.log(count); // Here count is not updated yet!
         setCount((currentCount) => currentCount + 1);
-        setCount((currentCount) => currentCount + 1);
+        // They won't cause new renders!
+        // These three things will batch together!
 
-        // Don't do this!
+        // Don't do this! if we depend on the previous version of the state!
         // setCount(count + 1);
         // setCount(count + 1);
         // // Here count is still same! it didn't change
