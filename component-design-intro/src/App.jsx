3542426307feda8b74aea9c1757b043877f0a39d
shtanriverdi <shtanriverdi@yahoo.com>
2023-11-14 14:22:38 +0300

    Passing a function as a prop

--- component-design-intro/src/App.jsx
+++ component-design-intro/src/App.jsx
@@ -7,11 +7,17 @@ function lessThan4(dice) {
   return sum(dice) < 4;
 }
 
+function allSameValue(dice) {
+  return dice.every(val => val === dice[0]);
+}
+
 function App() {
   return (
     <>
       {/* Pass a function as prop */}
       <LuckyN winCheck={lessThan4} />
+      <LuckyN winCheck={allSameValue} />
+      <LuckyN winCheck={allSameValue} numDice={3} />
       {/* <LuckyN numDice={3} goal={6} /> */}
       {/* <Dice dice={[3, 6, 1]} />
       <Dice dice={[5, 1, 2, 3]} color='orange' />
