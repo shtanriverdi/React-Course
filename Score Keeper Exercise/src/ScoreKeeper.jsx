1d487bb828d8d0ed05ca484dafdeef2083cfa6eb
shtanriverdi <shtanriverdi@yahoo.com>
2023-11-14 09:29:38 +0300

    Add updateScores by creating a copy of array

--- Score Keeper Exercise/src/ScoreKeeper.jsx
+++ Score Keeper Exercise/src/ScoreKeeper.jsx
@@ -1,8 +1,36 @@
+import { useState } from "react";
+
 export default function ScoreKeeper({ numOfPlayers, finalScore }) {
+    const [scores, setScores] = useState(Array(numOfPlayers).fill(0));
+
+    const updateScores = (index) => {
+        setScores((prevScores) => {
+            const newScores = [...prevScores];
+            newScores[index] += 1;
+            return newScores;
+        });
+    };
+
+    const resetScores = () => {
+        setScores(Array(numOfPlayers).fill(0));
+    };
+
     return (
-        <div>
+        <>
             <div>Score Keeper</div>
-            <button>Reset</button>
-        </div>
+            <ul>
+                {
+                    scores.map((score, index) => {
+                        return (
+                            <li key={index}>Player {index + 1}: {score}
+                                <button onClick={() => updateScores(index)}>+1</button>
+                                {(score === finalScore) && 'WINNER'}
+                            </li>
+                        );
+                    })
+                }
+            </ul>
+            <button onClick={resetScores}>Reset</button>
+        </>
     );
 }
