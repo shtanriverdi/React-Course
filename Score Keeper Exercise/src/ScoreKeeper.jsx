131f37ece70dfb5644da8227546eea7da7c5f83d
shtanriverdi <shtanriverdi@yahoo.com>
2023-11-14 08:57:01 +0300

    Create ScoreKeeper Component

---
+++ Score Keeper Exercise/src/ScoreKeeper.jsx
@@ -0,0 +1,8 @@
+export default function ScoreKeeper({ numOfPlayers, finalScore }) {
+    return (
+        <div>
+            <div>Score Keeper</div>
+            <button>Reset</button>
+        </div>
+    );
+}
