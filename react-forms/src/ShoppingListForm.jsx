de87c65abdda9cf412d87c760ae3e896634857e4
shtanriverdi <shtanriverdi@yahoo.com>
2023-11-15 20:24:56 +0300

    Add quantity input

--- react-forms/src/ShoppingListForm.jsx
+++ react-forms/src/ShoppingListForm.jsx
@@ -16,6 +16,7 @@ export default function ShoppingListForm() {
 
     return (
         <form>
+            <h1>Product is {formData.product} and Quantity: {formData.quantity}</h1>
             <label htmlFor="product">Product</label>
             <input
                 type="text"
@@ -23,6 +24,15 @@ export default function ShoppingListForm() {
                 name="product"
                 id="product"
                 value={formData.product}
+                onChange={handleChange}
+            />
+            <label htmlFor="quantity">Quantity</label>
+            <input
+                type="number"
+                placeholder="1"
+                name="quantity"
+                id="quantity"
+                value={formData.quantity}
                 onChange={handleChange}
             />
         </form>
