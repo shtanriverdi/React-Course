d578d8700551083d335ff758c02cffce2ab2668b
shtanriverdi <shtanriverdi@yahoo.com>
2023-11-15 14:17:24 +0300

    Array.from fix for duplicate key warning

--- Passing State Updating Functions From Parent To Children Click Boxes/src/BoxGrid.jsx
+++ Passing State Updating Functions From Parent To Children Click Boxes/src/BoxGrid.jsx
@@ -2,14 +2,25 @@ import "./BoxGrid.css"
 import { v4 as uuid } from 'uuid';
 import Box from "./Box";
 
-export default function BoxGrid({ numOfBoxes = 3 }) {
-    const rowOfBoxes = Array(numOfBoxes).fill(
-        <Box key={uuid()} />
-    );
+function getGrid(numOfBoxes) {
+    /** Don't use Array.fill here since it will replicate the
+     * uuid() for each element! You will get duplicate key warning!
+     * Use Array.fill instead which will call a function on each element
+     */
+    const rowOfBoxes = Array.from(new Array(numOfBoxes), () => {
+        return <Box key={uuid()} />
+    });
 
-    const boxesGrid = new Array(numOfBoxes).fill(
-        <div key={uuid()}>{rowOfBoxes}</div>
-    );
+    const boxesGrid = Array.from(new Array(numOfBoxes), () => {
+        return <div key={uuid()}>{rowOfBoxes}</div>
+    });
+
+    return boxesGrid;
+}
+
+export default function BoxGrid({ numOfBoxes = 3 }) {
+
+    const boxesGrid = getGrid(numOfBoxes);
 
     return (
         <div className="BoxGrid">
