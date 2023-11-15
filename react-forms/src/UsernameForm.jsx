ff8ec36f44796927257804e35cf43d0af6172793
shtanriverdi <shtanriverdi@yahoo.com>
2023-11-15 19:37:44 +0300

    htmlFor

--- react-forms/src/UsernameForm.jsx
+++ react-forms/src/UsernameForm.jsx
@@ -2,9 +2,11 @@ import { useState } from "react";
 
 export default function UsernameForm() {
     const [username, setUsername] = useState("Melogenesis");
+    // React is controlling this component and automatically
+    // passes the event objects
     const updateUsername = (event) => {
         // 'event.target' refers to the input
-        setUsername(event.target.value)
+        setUsername(event.target.value);
     }
     return (
         <div>
@@ -12,11 +14,13 @@ export default function UsernameForm() {
             We set the value on the input to be 'username'
             in our state! This is the key part, we bind this
             input with the state */}
+            <label htmlFor="username">Username:</label>
             <input
+                id="username"
                 value={username}
                 type="text"
                 placeholder="username"
-                onChange={(event) => updateUsername(event)}
+                onChange={updateUsername}
             />
             <button>Submit</button>
         </div>
