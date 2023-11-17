44489032e1b3883e66f796409491685646d1dbad
shtanriverdi <shtanriverdi@yahoo.com>
2023-11-17 10:59:42 +0300

    Why re-render solution, added main.jsx

--- The useEffect Hook/src/main.jsx
+++ The useEffect Hook/src/main.jsx
@@ -4,6 +4,11 @@ import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  /*
+  Your components will re-render an extra time to find bugs caused by impure rendering.
+  Your components will re-run Effects an extra time to find bugs caused by missing Effect cleanup.
+  Your components will be checked for usage of deprecated APIs.
+  */
   <React.StrictMode>
     <App />
   </React.StrictMode>,
