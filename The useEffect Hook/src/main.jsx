597a6ecfe1f0149acd42c1cb29cbae7ef6e80e02
shtanriverdi <shtanriverdi@yahoo.com>
2023-11-17 11:01:08 +0300

    A note about strictMode

--- The useEffect Hook/src/main.jsx
+++ The useEffect Hook/src/main.jsx
@@ -8,6 +8,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   Your components will re-render an extra time to find bugs caused by impure rendering.
   Your components will re-run Effects an extra time to find bugs caused by missing Effect cleanup.
   Your components will be checked for usage of deprecated APIs.
+
+  If you remove the strictMode, it will render only once properly!
+  But do that only in production!
   */
   <React.StrictMode>
     <App />
