eebbd154106afd1627413d47cb68ddde07b6e467
shtanriverdi <shtanriverdi@yahoo.com>
2023-11-20 14:51:15 +0300

    Random color fix

--- react-todos/src/AddTodo.jsx
+++ react-todos/src/AddTodo.jsx
@@ -20,6 +20,10 @@ export default function AddTodo({ onAddItem }) {
     const handleSubmit = (event) => {
         console.log(event.target, "Add ITEM");
         onAddItem(item);
+        // Change the background color for the next new item
+        setItem((prevItem) => {
+            return { ...prevItem, backgroundColor: getRandomColor() };
+        });
     }
 
     const handleChange = (event) => {
